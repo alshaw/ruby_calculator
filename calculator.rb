056046318347f0c5f2a388b12ebbb1f5d7c7ae8f
Allison Shaw <shaw.allison91@gmail.com>
2017-08-16 17:32:22 -0600

    [added quit and clear option]

--- calculator.rb
+++ calculator.rb
@@ -7,17 +7,34 @@ require 'colorize'
 
 def start
   puts 'Welcome to the ruby calculator!'.colorize(:green)
+  puts "Type quit to exit at any time.".colorize(:red)
   puts 'What is the first number?'
-    number_1 = gets.to_i
+    number_1 = gets.strip
+    if /(\d)|(\d+).(\d+)/.match(number_1)
+       num1f = number_1.to_f
+    elsif /quit/.match(number_1)
+        exit
+    else
+       puts "Invalid Input. Please enter a number."
+    end
   puts 'What is the operator?'
     operator = gets.strip
+    if /quit/.match(number_1)
+        exit
+    end
   puts "What is the second number?"
-    number_2 = gets.to_i
-  calculator(number_1, number_2, operator)
+    number_2 = gets.strip
+    if  /(\d)|(\d+).(\d+)/.match(number_2)
+        num2f = number_2.to_f
+    elsif /quit/.match(number_1)
+        exit
+    else
+        puts "Invalid Input. Please enter a number."
+        start
+    end
 
-#   puts "#{number_1} #{@operators[num]} #{number_2}"
-#   puts "#{number_1 @operators[num] number_2}"
-end 
+  calculator(num1f, num2f, operator)
+end
 
 def calculator(left, right, operator)
 case operator
@@ -45,19 +62,24 @@ case operator
 else
   puts 'Operator % is not supported. I only support + - * / operators.'
     end
+
 second_pass(answer)
 end
 
 def second_pass(answer)
     puts 'What is the operator?'
     operator = gets.strip
+    if /quit/.match(operator)
+        exit
+    elsif /clear/.match(operator)
+        start
+    end
     puts "What is the second number?"
-    number_2 = gets.to_i
+    number_2 = gets.to_f
+    if /quit/.match(number_2)
+        exit
+    end
     calculator(answer, number_2, operator)
-
-
-
-
 end
 
 start
